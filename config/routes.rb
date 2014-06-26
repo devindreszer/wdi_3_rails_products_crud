cb274e5822f73f0ce4c2312f5e1eb3d13f187b71
Devin Dreszer <devindreszer@gmail.com>
2014-06-26 10:32:52 -0400

    Create songs#index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Path is '/products'
   # Route to the ProductsController#index
   get '/products', to: 'products#index'
+  get '/songs', to: 'songs#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
