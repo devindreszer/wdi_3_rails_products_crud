dbed7d1ca2f1ade91721f64edb4a8aeb2b66c2d0
Devin Dreszer <devindreszer@gmail.com>
2014-06-26 09:45:58 -0400

    Create product seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+puts "Creating 20 Products"
+product_count = 20
+
+product_count.times do |i|
+  Product.create!(
+    name: "Product #{i}",
+    description: "Description #{i}",
+    price: rand(100) + (rand (100)/100.to_f).round(2)
+  )
+end
